f8b75ca0cc2469de37b2e93c4ccfa89314b19fbe
ibariens <bart.ariens@divimove.com>
2014-04-10 15:19:32 +0200

    adjust code to support all regions, see #1
    Did not notice that there were some duplicate patterns for region_wide tax numbers. Therefore needed to adjust the code drastically. However all patterns are now included in the tax_table, last to do is to update the tests and see if everything passes.

--- lib/steuernummer.rb
+++ lib/steuernummer.rb
@@ -21,42 +21,97 @@ class Steuernummer
   #   Steuernummer: (String)
 
 
-  def initialize(tax_string)
+  def initialize(tax_string, region = 'unknown')
     raise("Please use a String as argument") if tax_string.class != String
+    unless region == 'unknown' || (Steuernummer::TaxTable.tax_regions.include? region)
+      raise("unknown region, please use one of the following #{Steuernummer::TaxTable.tax_regions.join(', ')}")
+    end
 
-    @tax_string = tax_string
-    @tax_rule = get_tax_rule(tax_string)
+    @tax_string          = tax_string
+    @provided_region     = region
+    @tax_rules           = get_tax_rules(tax_string)
   end
 
-  def get_tax_rule(tax_string)
-    Steuernummer::TaxTable.data.each do |tax_rule|
-      if tax_string =~ tax_rule[:match_pattern]
-        tax_rule
-        break
+  def get_tax_rules(tax_string)
+    tax_rules = Array.new
+    Steuernummer::TaxTable.tax_rules.each do |tax_rule|
+      if tax_string =~ tax_rule[:match_pattern_region]
+        tax_rules << tax_rule.merge(:type => :region)
+      elsif tax_string =~ tax_rule[:match_pattern_country]
+        tax_rules << tax_rule.merge(:type => :country)
       end
     end
-    # If no tax rule found, return nil
-    nil
+    tax_rules
   end
 
   def is_valid?
-    !@tax_rule.nil?
+    if @provided_region != 'unknown'
+      regions = @tax_rules.map {|x| x[:region]}
+      regions.include? @provided_region
+    else
+      !@tax_rules.empty?
+    end
   end
 
-  # (German: Bündesland)
-  def region
-    if is_valid?
-      @tax_rule[:region]
+  def get_number_type
+    # returns :country or :region
+    tax_rule = @tax_rules.uniq{|x| x[:type]}
+    if tax_rule.count > 1
+      raise("both country and region type found, should be impossible!")
+    else
+      tax_rule.first[:type]
     end
   end
 
   # (German: Vereinheitlichtes Bundesschema)
-  def country_wide_code
-    if is_valid?
-      match_data = @tax_string.match @tax_rule[:match_pattern]
-      match_data_array = match_data.to_a
-      match_data_array.shift
-      country_wide = @tax_rule[:to_country_wide].call *match_data_array
+  def country_wide_number
+    return nil if !is_valid?
+    if get_number_type == :country
+      {:number => @tax_string, :region => @tax_rules.first[:region]}
+    elsif (@tax_rules.count > 1) && (@provided_region != 'unknown')
+      tax_rule = get_tax_rule_on_region
+
+      match_data = @tax_string.match tax_rule[:match_pattern_region]
+      arguments_array = match_data.to_a
+      arguments_array.shift
+
+      country_wide = tax_rule[:to_country_wide].call *arguments_array
+
+      {:number => country_wide, :region => tax_rule[:region]}
+    else
+      raise("Can't determine country wide number for a region wide
+             number without knowing the region.
+             Please pass one of the following as the second argument:
+             #{Steuernummer::TaxTable.tax_regions.join(', ')} ")
+    end
+  end
+
+  # (German: Bündesland)
+  def region_wide_number
+    return nil if !is_valid?
+    if get_number_type == :region
+      {:number => @tax_string, :region => @provided_region}
+    elsif get_number_type == :country
+      # country_wide_numbers only have one rule, so it's always the first
+      tax_rule = @tax_rules.first
+
+      match_data = @tax_string.match tax_rule[:match_pattern_country]
+      arguments_array = match_data.to_a
+      arguments_array.shift
+
+      region_wide = tax_rule[:to_region_wide].call *arguments_array
+
+      {:number => region_wide, :region => tax_rule[:region]}
+    end
+  end
+
+  def get_tax_rule_on_region
+    if @tax_rules.count > 1
+      possible_regions = @tax_rules.map {|x| x[:region]}
+      found_region_index = possible_regions.index @provided_region
+      @tax_rules[found_region_index]
+    else
+      @tax_rules.first
     end
   end
 end
--- lib/steuernummer/tax_table.rb
+++ lib/steuernummer/tax_table.rb
@@ -5,15 +5,107 @@ class Steuernummer::TaxTable
   # All values are based on: http://de.wikipedia.org/wiki/Steuernummer
   # To keep it a bit readible the variable names are kept the same as
   # in the wikipedia entry.
+    def self.tax_regions
+      Steuernummer::TaxTable.tax_rules.map {|x| x[:region]}
+    end
 
-    def self.data
+
+    def self.tax_rules
       [{:region => 'Baden-Württemberg',
-        :match_pattern => /(?<ff>\d{2})(?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_region => /(?<ff>\d{2})(?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /28(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff}#{bbb}/#{uuuu}#{p}"},
         :to_country_wide => lambda {|ff,bbb,uuuu,p| "28#{ff}0#{bbb}#{uuuu}#{p}"}},
 
-       {:region => 'Baden-Wurttemberg2',
-        :match_pattern => /(?<fgs>\d{2})(?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
-        :to_country_wide => lambda {|ff,bbb,uuuu,p| "28#{ff}0#{bbb}#{uuuu}#{p}"}}
+       {:region => 'Bayern',
+        :match_pattern_region => /(?<fff>\d{3})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /9(?<fff>\d{3})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|fff,bbb,uuuu,p| "#{fff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|fff,bbb,uuuu,p| "9#{fff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Berlin',
+        :match_pattern_region => /(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /11(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "11#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Brandenburg',
+        :match_pattern_region => /0(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /30(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "0#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "30#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Bremen',
+        :match_pattern_region => /(?<ff>\d{2})\s(?<bbb>\d{3})\s(?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /24(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff} #{bbb} #{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "24#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Hamburg',
+        :match_pattern_region => /(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /22(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "22#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Hessen',
+        :match_pattern_region => /0(?<ff>\d{2})\s(?<bbb>\d{3})\s(?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /26(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "0#{ff} #{bbb} #{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "26#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Mecklenburg-Vorpommern',
+        :match_pattern_region => /0(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /40(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "0#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "40#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Niedersachsen',
+        :match_pattern_region => /(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /23(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "23#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Nordrhein-Westfalen',
+        :match_pattern_region => /(?<fff>\d{3})[\/](?<bbbb>\d{4})[\/](?<uuu>\d{3})(?<p>\d{1})/,
+        :match_pattern_country => /5(?<fff>\d{3})0(?<bbbb>\d{4})(?<uuu>\d{3})(?<p>\d{1})/,
+        :to_region_wide => lambda {|fff,bbbb,uuu,p| "#{fff}/#{bbbb}/#{uuu}#{p}"},
+        :to_country_wide => lambda {|fff,bbbb,uuu,p| "5#{fff}0#{bbbb}#{uuu}#{p}"}},
+
+       {:region => 'Rheinland-Pfalz',
+        :match_pattern_region => /(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})[\/](?<p>\d{1})/,
+        :match_pattern_country => /27(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff}/#{bbb}/#{uuuu}/#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "27#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Saarland',
+        :match_pattern_region => /0(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /10(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "0#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "10#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Sachsen',
+        :match_pattern_region => /2(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /32(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "2#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "32#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Sachsen-Anhalt',
+        :match_pattern_region => /1(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /31(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "1#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "31#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Schleswig-Holstein',
+        :match_pattern_region => /(?<ff>\d{2})\s(?<bbb>\d{3})\s(?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /21(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "#{ff} #{bbb} #{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "21#{ff}0#{bbb}#{uuuu}#{p}"}},
+
+       {:region => 'Thüringen',
+        :match_pattern_region => /1(?<ff>\d{2})[\/](?<bbb>\d{3})[\/](?<uuuu>\d{4})(?<p>\d{1})/,
+        :match_pattern_country => /41(?<ff>\d{2})0(?<bbb>\d{3})(?<uuuu>\d{4})(?<p>\d{1})/ ,
+        :to_region_wide => lambda {|ff,bbb,uuuu,p| "1#{ff}/#{bbb}/#{uuuu}#{p}"},
+        :to_country_wide => lambda {|ff,bbb,uuuu,p| "41#{ff}0#{bbb}#{uuuu}#{p}"}}
       ]
    end
 end
